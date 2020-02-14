e136518d081f0e67d65146471f5e79762a28bb11
Jacob Stow <jacobstow@hotmail.com>
2020-02-14 14:13:32 +0000

    missing header tag

--- page.php
+++ page.php
@@ -6,7 +6,7 @@
 
 		<?php if (have_posts()): while (have_posts()) : the_post(); ?>
 
-			<article id="post-<?php the_ID(); ?>
+			<article id="post-<?php the_ID(); ?>">
 			<?php /*<article> */ ?>
 
 				<header class="intro clear">
